6050ac353ad7ad50d6ec74e5677956f24c4498bf
Danylo Senchyshyn <danylo.senchyshyn@student.tuke.sk>
2024-10-15 16:06:27 +0200

    Update lqueue.h

--- 04_lqueue/lqueue.h
+++ 04_lqueue/lqueue.h
@@ -1,26 +1,25 @@
-#ifndef LQUEUE_H
-#define LQUEUE_H
+    typedef int TElem;
 
-typedef int TElem;
+    #ifndef LQUEUE_H_INCLUDED
+    #define LQUEUE_H_INCLUDED
 
-struct QueueNode {
-    TElem data;
-    struct QueueNode* next;
-};
+    struct Node;
+    struct LnkQueue;
+    typedef struct Node *PtrToNode;
+    typedef struct LnkQueue *PtrToQueue;
+    typedef PtrToNode PNode;
+    typedef PtrToQueue LQueue;
 
-typedef struct {
-    struct QueueNode* front;
-    struct QueueNode* rear;
-} LQueue;
+    int IsEmptyQueue( LQueue Q );
+    LQueue CreateQueue( void );
+    void RemoveQueue( LQueue *PQ );
+    void MakeEmptyQueue( LQueue Q );
+    void Enqueue( TElem X, LQueue Q );
+    TElem Front( LQueue Q );
+    void Dequeue( LQueue Q );
+    TElem FrontAndDequeue( LQueue Q );
+    void PrintQueue( LQueue Q );
 
-int IsEmptyQueue(LQueue* Q);
-LQueue* CreateQueue(void);
-void RemoveQueue(LQueue* Q);
-void MakeEmptyQueue(LQueue* Q);
-void Enqueue(TElem X, LQueue* Q);
-TElem Front(LQueue* Q);
-void Dequeue(LQueue* Q);
-TElem FrontAndDequeue(LQueue* Q);
-void PrintQueue(LQueue* Q);
+    void MyPrintf(const char *format, ...);
 
-#endif
+    #endif // LQUEUE_H_INCLUDED
