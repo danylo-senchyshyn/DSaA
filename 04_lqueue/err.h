d6102689bdadb942e064c9df08d2f45f56c8a3c3
Danylo Senchyshyn <danylo.senchyshyn@student.tuke.sk>
2024-10-15 16:06:14 +0200

    Update err.h

--- 04_lqueue/err.h
+++ 04_lqueue/err.h
@@ -1,9 +1,4 @@
-#ifndef ERR_H
-#define ERR_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ERROR(msg) { printf("Error: %s\n", msg); exit(1); }
-
-#endif
+#define Error( errmsg )       printf( "\nError - %s\n", errmsg ), exit(1)
