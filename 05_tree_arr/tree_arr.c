33dd51146d7f8d7e67329246277f7b9828cc94e8
Danylo Senchyshyn <danylo.senchyshyn@student.tuke.sk>
2024-10-22 11:05:22 +0200

    22.10 - 11:05

--- 05_tree_arr/tree_arr.c
+++ 05_tree_arr/tree_arr.c
@@ -9,11 +9,15 @@ struct TreeRecord {
     int *Array;
 };
 
+int GetMaxSize(Tree T) {
+    return pow(2, T->MaxLevel + 1);
+}
+
 Tree CreateTree(int MaxLevel) {
     Tree T = malloc(sizeof(struct TreeRecord));
     if (T == NULL) Error("Out of space!!!");
 
-    int size = pow(2, MaxLevel + 1);
+    int size = GetMaxSize(T);
     T->Array = malloc(size * sizeof(int));
     if (T->Array == NULL) Error("Out of space!!!");
 
@@ -34,17 +38,15 @@ void DisposeTree(Tree T) {
 
 void MakeEmpty(Tree T) {
     if (T == NULL) Error("No tree!");
-    for (int i = 1; i < pow(2, T->MaxLevel + 1); i++) {
+    for (int i = 1; i < GetMaxSize(T); i++) {
         T->Array[i] = 0;
     }
 }
 
 void Insert(Tree T, int Index, int Elem) {
     if (T == NULL) Error("No tree!");
-    if (Index < 1 || Index >= pow(2, T->MaxLevel + 1))
-        Error("Bad index!");
-    else
-        T->Array[Index] = Elem;
+    if (Index < 1 || Index >= GetMaxSize(T)) Error("Bad index!");
+    T->Array[Index] = Elem;
 }
 
 void PrintTree(Tree T) {
@@ -75,8 +77,8 @@ void PrintRow(Tree T, int Level) {
     if (T == NULL) Error("No tree!");
     if (Level < 1 || Level > T->MaxLevel) Error("Invalid level!");
 
-    int start = pow(2, Level) - 1;
-    int end = start + pow(2, Level) - 1;
+    int start = pow(2, Level - 1);
+    int end = pow(2, Level) - 1;
 
     for (int i = start; i <= end; i++) {
         if (T->Array[i] != 0)
@@ -89,22 +91,16 @@ void PrintRow(Tree T, int Level) {
 
 void PrintSubtree(Tree T, int Index) {
     if (T == NULL) Error("No tree!");
-    if (Index < 1 || Index >= pow(2, T->MaxLevel + 1)) Error("Bad index!");
+    if (Index < 1 || Index >= GetMaxSize(T)) Error("Bad index!");
 
-    if (T->Array[Index] != 0)
-        printf("%02d ", T->Array[Index]);
+    if (T->Array[Index] == 0) return;
+    printf("%02d ", T->Array[Index]);
 
     int left = 2 * Index;
     int right = 2 * Index + 1;
 
-    if (left < pow(2, T->MaxLevel + 1) && T->Array[left] != 0)
+    if (left < GetMaxSize(T))
         PrintSubtree(T, left);
-    if (right < pow(2, T->MaxLevel + 1) && T->Array[right] != 0)
+    if (right < GetMaxSize(T))
         PrintSubtree(T, right);
-}
-
-void PrintSpaces(int Count) {
-    for (int i = 0; i < Count; i++) {
-        putchar(' ');
-    }
 }
