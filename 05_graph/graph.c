890279dd299185c8f7a2f2e4fde6a44b8178ca86
Danylo Senchyshyn <danylo.senchyshyn@student.tuke.sk>
2024-10-22 16:32:09 +0200

    22.10 - 16:32

--- 05_graph/graph.c
+++ 05_graph/graph.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "err.h"
 #include "graph.h"
 #include "lqueue.h"
@@ -13,14 +12,10 @@ struct GraphRecord
 
 Graph CreateGraph(int NodesCount) {
     Graph G = malloc(sizeof(struct GraphRecord));
-    if (G == NULL) Error("Memory allocation failed!");
+    if (G == NULL) Error("Out of space!");
+
     G->nodes = NodesCount;
-    for (int i = 0; i < NodesCount; i++) {
-        G->visited[i] = 0;
-        for (int j = 0; j < NodesCount; j++) {
-            G->adj[i][j] = 0;
-        }
-    }
+
     return G;
 }
 
@@ -29,9 +24,10 @@ void DisposeGraph(Graph G) {
 }
 
 void buildadjm(Graph G) {
-    for (int i = 0; i < G->nodes; i++) {
-        for (int j = 0; j < G->nodes; j++) {
-            printf("Введите 1, если есть ребро от %d до %d, иначе 0: ", i, j);
+    int i, j;
+    for (i = 0; i < G->nodes; i++) {
+        for (j = 0; j < G->nodes; j++) {
+            printf("enter 1 if there is an edge from %d to %d, otherwise enter 0 \n", i, j);
             scanf("%d", &(G->adj[i][j]));
         }
     }
@@ -40,112 +36,128 @@ void buildadjm(Graph G) {
 void printadjm(Graph G) {
     for (int i = 0; i < G->nodes; i++) {
         for (int j = 0; j < G->nodes; j++) {
-            printf("%d ", G->adj[i][j]);
+            printf(" %d", G->adj[i][j]);
+            putchar('\n');
         }
-        putchar('\n');
     }
 }
 
 void ClearVisited(Graph G) {
-    for (int n = 0; n < G->nodes; n++) {
+    for (int n = 0; n < G->nodes; n++)
         G->visited[n] = 0;
-    }
 }
 
+// 3 stavy - 0 (not found), 1 (opened), 2 (closed)
 void dfs(Graph G, int v0) {
     if (G == NULL) {
-        Error("Graph is not defined!");
+        Error("Graph is NULL!");
+        return;
     }
+
     ClearVisited(G);
-    dfs2(G, v0);
+    dfs2(G,v0);
 }
 
 void dfs2(Graph G, int v) {
-    printf("Посещаем узел: %d\n", v);
+    printf("The node opened: %d\n", v);
+
     G->visited[v] = 1;
+
     for (int w = 0; w < G->nodes; w++) {
         if (G->adj[v][w] == 1 && G->visited[w] == 0) {
             dfs2(G, w);
         }
+        G->visited[v] = 2;
     }
-    G->visited[v] = 2;
 }
 
 int find_not_visited_node(Graph G) {
     for (int i = 0; i < G->nodes; i++) {
-        if (G->visited[i] == 0) return i;
+        for (int j = 0; j < G->nodes; j++) {
+            if(G->adj[i][j] == 1 && G->visited[j] == 0) {
+                return i;
+            }
+        }
     }
     return -1;
 }
 
 void dfsst(Graph G, int v0) {
-    if (G == NULL) {
-        Error("Graph is not defined!");
+    if(G == NULL) {
+        Error("No graph");
+        return;
     }
     ClearVisited(G);
+
     int not_visited_node = v0;
     do {
         dfsst2(G, not_visited_node);
-    } while ((not_visited_node = find_not_visited_node(G)) != -1);
+    } while((not_visited_node = find_not_visited_node(G)) != -1);
 }
 
 void dfsst2(Graph G, int v) {
     for (int w = 0; w < G->nodes; w++) {
         if (G->adj[v][w] == 1 && G->visited[w] == 0) {
-            printf("Ребро: (%d, %d)\n", v, w);
+            printf("Edge: (%d,%d)\n", v, w);
             dfsst2(G, w);
         }
+        G->visited[v] = 2;
     }
-    G->visited[v] = 2;
 }
 
 void bfs(Graph G, int v0) {
-    if (G == NULL) {
-        Error("Graph is not defined!");
+    if(G == NULL) {
+        Error("No graph");
+        return;
     }
     ClearVisited(G);
     LQueue Q = CreateQueue();
     Enqueue(v0, Q);
-    G->visited[v0] = 1;
-    printf("Посещаем узел: %d\n", v0);
+    G->visited[v0] = 2;
+    printf("The node opened: %d\n", v0);
 
-    while (!IsEmptyQueue(Q)) {
+    while(!IsEmptyQueue(Q)) {
         int node = FrontAndDequeue(Q);
+
         for (int i = 0; i < G->nodes; i++) {
-            if (G->adj[node][i] == 1 && G->visited[i] == 0) {
+            if(G->adj[node][i] == 1 && G->visited[i] == 0) {
                 Enqueue(i, Q);
-                printf("Посещаем узел: %d\n", i);
-                G->visited[i] = 1;
+                printf("The node opened: %d\n", i);
+                G->visited[i] = 2;
             }
         }
-        G->visited[node] = 2;
     }
 }
 
 void bfsst(Graph G, int v0) {
     if (G == NULL) {
-        Error("Graph is not defined!");
+        Error("No graph");
+        return;
     }
     ClearVisited(G);
     LQueue Q = CreateQueue();
     Enqueue(v0, Q);
-    G->visited[v0] = 1;
+    G->visited[v0] = 2;
+
+    int isEnd;
 
     do {
         while (!IsEmptyQueue(Q)) {
             int node = FrontAndDequeue(Q);
+
             for (int i = 0; i < G->nodes; i++) {
                 if (G->adj[node][i] == 1 && G->visited[i] == 0) {
                     Enqueue(i, Q);
-                    printf("Ребро: (%d, %d)\n", node, i);
-                    G->visited[i] = 1;
+                    printf("Edge: (%d,%d)\n", node, i);
+                    G->visited[i] = 2;
                 }
             }
         }
-        int isEnd = find_not_visited_node(G);
-        if (isEnd != -1) {
+
+        if ((isEnd = find_not_visited_node(G)) != -1) {
             Enqueue(isEnd, Q);
-            G->visited[isEnd] = 1;
+            G->visited[isEnd] = 2;
         }
-    } while (find_not_visited_node(G) != -1);
+
+    } while (isEnd != -1);
 }
