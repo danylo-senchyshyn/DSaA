bc10ba6b94015640e35639967ac7e13c651bf302
Danylo Senchyshyn <danylo.senchyshyn@student.tuke.sk>
2024-10-29 12:40:15 +0100

    o

--- 05_graph/graph.c
+++ 05_graph/graph.c
@@ -8,7 +8,6 @@ struct GraphRecord {
     int visited[max];
     int nodes;
 };
-
 Graph CreateGraph(int NodesCount) {
     Graph G;
 
@@ -19,14 +18,9 @@ Graph CreateGraph(int NodesCount) {
 
     return G;
 }
-
-
 void DisposeGraph(Graph G) {
     free(G);
 }
-
-
-/* a function to build adjacency matrix of a graph */
 void buildadjm(Graph G) {
     for (int i = 0; i < G->nodes; i++)
         for (int j = 0; j < G->nodes; j++) {
@@ -34,7 +28,6 @@ void buildadjm(Graph G) {
             scanf("%d", &(G->adj[i][j]));
         }
 }
-
 void printadjm(Graph G) {
     for (int i = 0; i < G->nodes; i++) {
         for (int j = 0; j < G->nodes; j++)
@@ -42,13 +35,19 @@ void printadjm(Graph G) {
         putchar('\n');
     }
 }
-
 void ClearVisited(Graph G) {
     for (int n = 0; n < G->nodes; n++)
         G->visited[n] = 0;
 }
+int find_not_visited_node(Graph G) {
+    for (int i = 0; i < G->nodes; i++) {
+        if (G->visited[i] == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
 
-// 3 stavy - 0 (not found), 1 (opened), 2 (closed)
 void dfs(Graph G, int v0) {
     if (G == NULL) {
         Error("No graph");
@@ -56,24 +55,28 @@ void dfs(Graph G, int v0) {
     }
     ClearVisited(G);
     dfs2(G, v0);
+
+    for (int i = 0; i < G->nodes; i++) {
+        if (G->visited[i] == 0) {
+            G->visited[i] = 2;
+            printf("Isolated node closed: %d\n", i);
+        }
+    }
 }
 
 void dfs2(Graph G, int v) {
+    if (G->visited[v] != 0) return;
     printf("The node opened: %d\n", v);
-    for (int w = 0; w < G->nodes; w++) {
-        if (G->adj[v][w] == 1 && G->visited[w] == 0)
-            dfs2(G, w);
-        G->visited[v] = 2;
-    }
-}
+    G->visited[v] = 1;
 
-int find_not_visited_node(Graph G) {
-    for (int i = 0; i < G->nodes; i++) {
-        for (int j = 0; j < G->nodes; j++) {
-            if (G->adj[i][j] == 1 && G->visited[j] == 0) return i;
+    for (int w = 0; w < G->nodes; w++) {
+        if (G->adj[v][w] == 1 && G->visited[w] == 0) {
+            dfs2(G, w);
         }
     }
-    return -1;
+
+    G->visited[v] = 2;
+    printf("The node closed: %d\n", v);
 }
 
 void dfsst(Graph G, int v0) {
@@ -82,22 +85,26 @@ void dfsst(Graph G, int v0) {
         return;
     }
     ClearVisited(G);
+    dfsst2(G, v0);
 
-    int not_visited_node = v0;
-    do {
+    int not_visited_node;
+    while ((not_visited_node = find_not_visited_node(G)) != -1) {
         dfsst2(G, not_visited_node);
-    } while ((not_visited_node = find_not_visited_node(G)) != -1);
+    }
 }
 
-
 void dfsst2(Graph G, int v) {
+    if (G->visited[v] != 0) return;
+    G->visited[v] = 1;
+
     for (int w = 0; w < G->nodes; w++) {
         if (G->adj[v][w] == 1 && G->visited[w] == 0) {
             printf("Edge: (%d,%d)\n", v, w);
             dfsst2(G, w);
         }
-        G->visited[v] = 2;
     }
+
+    G->visited[v] = 2;
 }
 
 void bfs(Graph G, int v0) {
@@ -105,10 +112,11 @@ void bfs(Graph G, int v0) {
         Error("No graph");
         return;
     }
+
     ClearVisited(G);
     LQueue Q = CreateQueue();
     Enqueue(v0, Q);
-    G->visited[v0] = 2;
+    G->visited[v0] = 1;
     printf("The node opened: %d\n", v0);
 
     while (!IsEmptyQueue(Q)) {
@@ -117,8 +125,8 @@ void bfs(Graph G, int v0) {
         for (int i = 0; i < G->nodes; i++) {
             if (G->adj[node][i] == 1 && G->visited[i] == 0) {
                 Enqueue(i, Q);
+                G->visited[i] = 1;
                 printf("The node opened: %d\n", i);
-                G->visited[i] = 2;
             }
         }
     }
@@ -129,10 +137,11 @@ void bfsst(Graph G, int v0) {
         Error("No graph");
         return;
     }
+
     ClearVisited(G);
     LQueue Q = CreateQueue();
     Enqueue(v0, Q);
-    G->visited[v0] = 2;
+    G->visited[v0] = 1;
 
     int isEnd;
 
@@ -143,15 +152,16 @@ void bfsst(Graph G, int v0) {
             for (int i = 0; i < G->nodes; i++) {
                 if (G->adj[node][i] == 1 && G->visited[i] == 0) {
                     Enqueue(i, Q);
-                    printf("Edge: (%d,%d)\n", node, i);
-                    G->visited[i] = 2;
+                    G->visited[i] = 1;
+                    printf("Edge: (%d, %d)\n", node, i);
                 }
             }
         }
 
-        if ((isEnd = find_not_visited_node(G)) != -1) {
+        isEnd = find_not_visited_node(G);
+        if (isEnd != -1) {
             Enqueue(isEnd, Q);
-            G->visited[isEnd] = 2;
+            G->visited[isEnd] = 1;
         }
 
     } while (isEnd != -1);
